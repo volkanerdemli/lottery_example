f16c9147b63b4d1c4338f8edf2ebedf56942fabb
volkanerdemli <76881009+volkanerdemli@users.noreply.github.com>
2022-04-08 23:57:26 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+-- Users can enter lottery with ETH based on a USD fee
+-- An admin will choose when the lottery is over
+-- The lottery will select a random winner
+
+Test this
+
+1. 'mainnet-fork'
+2. 'development' with mocks
+3. 'testnet'
---
+++ scripts/deploy_lottery.py
@@ -0,0 +1,54 @@
+from distutils.command.config import config
+from eth_utils import address
+from scripts.helpful_scripts import get_account, get_contract, fund_with_link
+from brownie import Lottery, network, config
+import time
+
+def deploy_lottery():
+    account = get_account()
+    lottery = Lottery.deploy(
+        get_contract("eth_usd_price_feed").address,
+        get_contract("vrf_coordinator").address,
+        get_contract("link_token").address,
+        config["networks"][network.show_active()]["fee"],
+        config["networks"][network.show_active()]["keyhash"],
+        {"from": account},
+        publish_source = config["networks"][network.show_active()].get("verify", False)
+    )
+    print("deployed lottery") 
+    return lottery   
+
+
+def start_lottery():
+    account = get_account()
+    lottery = Lottery[-1]   
+    lottery.startLottery({"from": account})
+    print("Lottery is now started")
+
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    value = lottery.getEntranceFee() + 10000000
+    tx = lottery.enter({"from": account, "value": value})
+    print("Now, you have entered to the lottery")
+
+
+def end_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    # fund the contract
+    # then end the lottery
+    tx = fund_with_link(lottery.address)
+    tx.wait(1)
+    ending_transaction = lottery.endLottery({"from": account})
+    ending_transaction.wait(1)
+    time.sleep(60)
+    print(lottery.recentWinner()," is the new winner!")
+
+
+
+def main():
+    deploy_lottery()
+    start_lottery()
+    enter_lottery()
+    end_lottery()
